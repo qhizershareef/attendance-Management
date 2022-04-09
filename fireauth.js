9d48059d1316d395a7d3ec2d90cfdfa7d505964a
Qhizer Shareef <51617074+qhizershareef@users.noreply.github.com>
2022-04-09 01:58:45 -0700

    Update fireauth.js

--- fireauth.js
+++ fireauth.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAaguzRK6FNY01WzTrmaa3ml7LSUUFwsp8",
-    authDomain: "attendance-management-1c90a.firebaseapp.com",
-    projectId: "attendance-management-1c90a",
-    storageBucket: "attendance-management-1c90a.appspot.com",
-    messagingSenderId: "954568865734",
-    appId: "1:954568865734:web:fdaaaa259f791cc9dba4c7"
+
 };
 
   // Initialize Firebase
